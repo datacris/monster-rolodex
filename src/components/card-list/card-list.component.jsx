e17d953ffb0556f4f394eaf00439445202fae340
Cristian Zuluaga <cristianzuluaga@Cristians-MacBook-Pro.local>
2022-05-09 17:17:12 -0500

    monster rolodex firsdt commit

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,17 @@
+
+import './card-list.styles.css'
+import Card from '../card/card.componet'
+
+ const CardList =({monsters})=> {
+
+    return (
+      <div className='card-list'>
+        {monsters.map(monster => {
+          return <Card monster={monster} />
+        }
+      )}</div>
+    )
+  
+}
+
+export default CardList
